a340aa38f6b0c846e376c69de59892611e45d074
Arthur Vincent <arthurvincent.simon@gmail.com>
2013-09-11 17:20:19 +0800

    Renamed some files

---
+++ system/expressionengine/third_party/ci_multilanguage/language/english/ci_multilanguage_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+	if (!defined('BASEPATH'))
+	{
+		exit('No direct script access allowed');
+	}
+
+	$lang = array(
+		/* Required for ADD-ONS > MODULES page */
+
+		'ci_multilanguage_module_name'		 => 'CI Multilanguage Module',
+		'ci_multilanguage_module_description' => 'A simple module to be able to switch languages',
+		/* Required for CONTROL PANEL pages */
+
+		/* Required for FRONT END module */
+		'' => ''
+	);
+
+	/* End of file ci_multilanguage_lang.php */
+	/* Location: /system/expressionengine/third_party/ci_multilanguage/language/english/ci_multilanguage_lang.php */ 
